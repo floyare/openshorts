f5a780aff4150a4e4910ddf6a2307fbc893b9301
floyare <neptuneeex@gmail.com>
2025-04-20 19:46:05 +0200

    feat: added shadcn library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import node from '@astrojs/node';
 
 // https://astro.build/config
