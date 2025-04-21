00591707b0d53c13b3af2669f098db1961cc542a
floyare <neptuneeex@gmail.com>
2025-04-21 14:04:00 +0200

    refactor: changed /api/screen to GET

--- src/pages/api/screen.ts
+++ src/pages/api/screen.ts
@@ -1,21 +1,19 @@
 export const prerender = false;
+
+/* 
+    This API endpoint will return an Website screenshot when adding website to the db and store it.
+*/
+
 import puppeteer, { Browser } from 'puppeteer-core';
 import type { APIRoute } from 'astro';
 
-export const POST: APIRoute = async ({ request }) => {
+export const GET: APIRoute = async ({ params, request }) => {
     const BROWSERLESS_API_KEY = import.meta.env.BROWSERLESS_API_KEY;
     if (!BROWSERLESS_API_KEY) {
         return new Response('Missing BROWSERLESS_API_KEY environment variable', { status: 500 });
     }
 
-    let urlParam: string | undefined;
-    try {
-        const body = await request.json();
-        urlParam = body.body;
-    } catch {
-        return new Response('Invalid JSON body', { status: 400 });
-    }
-
+    let urlParam = new URL(request.url).searchParams.get('url')
     if (!urlParam) {
         return new Response('Missing url in request body', { status: 400 });
     }
