eb7f6ebc5ea1f93aebe8c9d7244615a2a22abbf5
floyare <neptuneeex@gmail.com>
2025-04-23 17:18:11 +0200

    feat: added prototype of pagination

--- src/components/website-browser.tsx
+++ src/components/website-browser.tsx
@@ -1,18 +1,53 @@
 import type { WebsiteType } from "@/types/website";
 import WebsiteItem from "./websites/website-item";
+import { useMemo, useState } from "react";
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 
 type BrowserProps = {
     entryWebsites: WebsiteType[],
     totalWebsites: number
 }
 
+const PAGE_SIZE = 1
+
 const WebsiteBrowser = ({ entryWebsites, totalWebsites }: BrowserProps) => {
+    const [page, setPage] = useState(1);
+    const totalPages = Math.ceil(totalWebsites / PAGE_SIZE);
+
+    const pagedWebsites = useMemo(() => entryWebsites.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE), [page, totalPages]);
+
     return (
         <section>
             <p>total: {totalWebsites}</p>
-            {entryWebsites.map((website) => (
+            {pagedWebsites.map((website) => (
                 <WebsiteItem website={website} key={website.id} />
             ))}
+            <Pagination className="mt-4">
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious
+                            onClick={() => setPage((p) => Math.max(1, p - 1))}
+                            isDisabled={page === 1}
+                        />
+                    </PaginationItem>
+                    {Array.from({ length: totalPages }, (_, i) => (
+                        <PaginationItem key={i + 1}>
+                            <PaginationLink
+                                isActive={page === i + 1}
+                                onClick={() => setPage(i + 1)}
+                            >
+                                {i + 1}
+                            </PaginationLink>
+                        </PaginationItem>
+                    ))}
+                    <PaginationItem>
+                        <PaginationNext
+                            onClick={() => setPage((p) => Math.min(totalPages, p + 1))}
+                            isDisabled={page === totalPages}
+                        />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </section>
     );
 }
