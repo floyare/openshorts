19e38f31b30912fdbdf72112b678abc7fc9feff8
floyare <neptuneeex@gmail.com>
2025-04-27 23:36:21 +0200

    style: working on browser styles

--- src/components/website-browser.tsx
+++ src/components/website-browser.tsx
@@ -6,6 +6,7 @@ import { PAGE_SIZE } from "@/lib/websites.core";
 import { debugLog } from "@/lib/log";
 import { actions } from "astro:actions";
 import { cn } from "@/lib/utils";
+import useDebounce from "@/hooks/useDebounce";
 
 type BrowserProps = {
     entryWebsites: WebsiteType[],
@@ -28,11 +29,14 @@ const WebsiteBrowser = ({ entryWebsites, totalWebsites, tags }: BrowserProps) =>
         tags: []
     })
 
+    const debouncedSearch = useDebounce(searchContent.search, 600)
+    const debouncedTags = useDebounce(searchContent.tags, 300)
+
     useEffect(() => {
         const fetchWebsites = async () => {
             try {
                 debugLog("DEBUG", "Fetching websites with page: ", page, " and search content: ", searchContent);
-                const data = await actions.searchWebsites({ page, search: searchContent.search, tags: searchContent.tags });
+                const data = await actions.searchWebsites({ page, search: debouncedSearch, tags: debouncedTags });
                 debugLog("DEBUG", "page: ", page, " - websites: ", data.data?.websites);
                 if (data.error || !data.data) {
                     debugLog("ERROR", "Failed to fetch websites: ", data.error);
@@ -47,74 +51,78 @@ const WebsiteBrowser = ({ entryWebsites, totalWebsites, tags }: BrowserProps) =>
         };
 
         startWebsitesLoading(() => fetchWebsites())
-    }, [page, searchContent]);
+    }, [page, debouncedSearch, debouncedTags]);
 
     return (
-        <section>
-            <p>total: {totalPages}</p>
-            <input
-                type="text"
-                placeholder="Search..."
-                className="bg-purple-300 mb-4"
-                value={searchContent.search}
-                onChange={(e) =>
-                    searchContentSet((p) => ({
-                        ...p,
-                        search: e.target.value,
-                    }))
-                }
-            />
-            <ul>
-                {
-                    tags.map((tag) => (
-                        <li>
-                            <input type="checkbox" id={tag.name} onChange={(e) => {
-                                if (e.target.checked) {
-                                    searchContentSet((p) => ({
-                                        ...p,
-                                        tags: [...p.tags, tag.name],
-                                    }))
-                                } else {
-                                    searchContentSet((p) => ({
-                                        ...p,
-                                        tags: p.tags.filter((t) => t !== tag.name),
-                                    }))
-                                }
-                            }} />
-                            <label htmlFor={tag.name} className="ml-2">{tag.name}</label>
-                        </li>
-                    ))
-                }
-            </ul>
-            {filteredWebsites.map((website) => (
-                <WebsiteItem website={website} key={website.id} />
-            ))}
-            <Pagination className={cn("transition-all", websitesLoading ? "opacity-70 pointer-events-none grayscale" : "")}>
-                <PaginationContent>
-                    <PaginationItem>
-                        <PaginationPrevious
-                            onClick={() => setPage((p) => Math.max(1, p - 1))}
-                            isDisabled={page === 1 || websitesLoading}
-                        />
-                    </PaginationItem>
-                    {Array.from({ length: totalPages }, (_, i) => (
-                        <PaginationItem key={i + 1}>
-                            <PaginationLink
-                                isActive={page === i + 1}
-                                onClick={() => setPage(i + 1)}
-                            >
-                                {i + 1}
-                            </PaginationLink>
+        <section className="flex items-start justify-between gap-6 w-full">
+            <aside className="bg-background-950 p-4 rounded-lg border-[1px] border-background-800">
+                <p>total: {totalPages}</p>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="bg-purple-300 mb-4"
+                    value={searchContent.search}
+                    onChange={(e) =>
+                        searchContentSet((p) => ({
+                            ...p,
+                            search: e.target.value,
+                        }))
+                    }
+                />
+                <ul>
+                    {
+                        tags.map((tag) => (
+                            <li>
+                                <input type="checkbox" id={tag.name} onChange={(e) => {
+                                    if (e.target.checked) {
+                                        searchContentSet((p) => ({
+                                            ...p,
+                                            tags: [...p.tags, tag.name],
+                                        }))
+                                    } else {
+                                        searchContentSet((p) => ({
+                                            ...p,
+                                            tags: p.tags.filter((t) => t !== tag.name),
+                                        }))
+                                    }
+                                }} />
+                                <label htmlFor={tag.name} className="ml-2">{tag.name}</label>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </aside>
+            <div>
+                {filteredWebsites.map((website) => (
+                    <WebsiteItem website={website} key={website.id} />
+                ))}
+                <Pagination className={cn("transition-all", websitesLoading ? "opacity-70 pointer-events-none grayscale" : "")}>
+                    <PaginationContent>
+                        <PaginationItem>
+                            <PaginationPrevious
+                                onClick={() => setPage((p) => Math.max(1, p - 1))}
+                                isDisabled={page === 1 || websitesLoading}
+                            />
                         </PaginationItem>
-                    ))}
-                    <PaginationItem>
-                        <PaginationNext
-                            onClick={() => setPage((p) => Math.min(totalPages, p + 1))}
-                            isDisabled={page === totalPages || websitesLoading}
-                        />
-                    </PaginationItem>
-                </PaginationContent>
-            </Pagination>
+                        {Array.from({ length: totalPages }, (_, i) => (
+                            <PaginationItem key={i + 1}>
+                                <PaginationLink
+                                    isActive={page === i + 1}
+                                    onClick={() => setPage(i + 1)}
+                                >
+                                    {i + 1}
+                                </PaginationLink>
+                            </PaginationItem>
+                        ))}
+                        <PaginationItem>
+                            <PaginationNext
+                                onClick={() => setPage((p) => Math.min(totalPages, p + 1))}
+                                isDisabled={page === totalPages || websitesLoading}
+                            />
+                        </PaginationItem>
+                    </PaginationContent>
+                </Pagination>
+            </div>
         </section>
     );
 }
