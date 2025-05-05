372f3ffab7cd570b743d0605f2ac8d1722641194
floyare <neptuneeex@gmail.com>
2025-05-05 09:18:24 +0200

    refactor: added check if url exist in uploadSchema

--- src/helpers/upload.helper.ts
+++ src/helpers/upload.helper.ts
@@ -1,9 +1,12 @@
+import { debugLog } from "@/lib/log";
 import { z } from "astro/zod";
 import { actions } from "astro:actions";
 
 export const uploadSchema = z.object({
     url: z.string().url({ message: "Invalid URL" })
         .refine(async (url: string): Promise<boolean> => {
+            if (!url) return false;
+            debugLog("ACTION", "Checking if website exists", url);
             const exists: boolean | undefined = (await actions.doesWebsiteExists({ url })).data;
             return exists === false
         }, { message: "Website already exists" }),
