9a3ec33f661b0955be978884b3f81c2ecc4c638f
floyare <neptuneeex@gmail.com>
2025-05-05 12:10:06 +0200

    fix: fixed doesWebsiteExists check on the server

--- src/helpers/upload.helper.ts
+++ src/helpers/upload.helper.ts
@@ -1,4 +1,5 @@
 import { debugLog } from "@/lib/log";
+import { doesWebsiteExists } from "@/lib/websites.core";
 import { z } from "astro/zod";
 import { actions } from "astro:actions";
 
@@ -6,8 +7,9 @@ export const uploadSchema = z.object({
     url: z.string().url({ message: "Invalid URL" })
         .refine(async (url: string): Promise<boolean> => {
             if (!url) return false;
-            debugLog("ACTION", "Checking if website exists", url);
-            const exists: boolean | undefined = (await actions.doesWebsiteExists({ url })).data;
+            const isServer = typeof window === "undefined";
+            debugLog("ACTION", "Checking if website exists", `(${isServer ? "SERVER" : "CLIENT"})`, url);
+            const exists: boolean | undefined = isServer ? await doesWebsiteExists(url) : (await actions.doesWebsiteExists({ url })).data;
             return exists === false
         }, { message: "Website already exists" }),
     description: z.string().min(10).max(200),
