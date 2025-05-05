979ac658e7dd83f97fd28079e1a6f6c8193fd69c
floyare <neptuneeex@gmail.com>
2025-05-05 12:45:17 +0200

    style: fixed layout bugs in website-item

--- src/components/websites/website-item.tsx
+++ src/components/websites/website-item.tsx
@@ -15,19 +15,16 @@ function WebsiteItem({ website }: WebsiteItemProps) {
 
     return (
         <div className="px-6 py-4 max-w-lg rounded-sm border-[1px] border-background-800 bg-background-950 w-full flex flex-col gap-2 grow relative">
-            <div className="grid grid-cols-[1fr_auto] gap-1 bg-accent-700">
-                <div className="flex flex-col gap-2 bg-yellow-200 col-span-1 w-full">
-                    <a href={website.url} target="_blank" className="flex items-center gap-4 cursor-pointer hover:bg-primary-700/20 transition-colors rounded-sm w-fit">
+            <div className="grid grid-cols-[1fr_auto] gap-1 bg-accent-700 !w-full">
+                <div className="flex flex-col gap-2 bg-yellow-200 w-full">
+                    <a href={website.url} target="_blank" className="flex items-center gap-4 cursor-pointer hover:bg-primary-700/20 transition-colors rounded-sm w-full">
                         <WebsiteIcon src={`https://s2.googleusercontent.com/s2/favicons?domain=${url}&sz=128`} alt={`${name} favicon`} size={48} />
                         <h2 className="font-bold text-2xl flex items-center gap-1 relative bg-green-300 w-full">
-                            <span className="truncate inline-block !w-[88%] bg-red-400">{name}</span>
+                            <span className="truncate inline-block !max-w-[160px] bg-red-400">{name}</span>
                             <ExternalLink className="text-text-600" size={18} />
                         </h2>
-                        {/* <div>
-                            <h3 className="text-xl font-semibold">{name}</h3>
-                        </div> */}
                     </a>
-                    <p className="w-full overflow-hidden text-ellipsis [display:-webkit-box] [-webkit-line-clamp:6] [-webkit-box-orient:vertical] bg-blue-300">
+                    <p className="w-full overflow-hidden text-ellipsis [display:-webkit-box] [-webkit-line-clamp:6] [-webkit-box-orient:vertical] bg-blue-300 break-words text-balance">
                         {description}
                     </p>
                     <div className="mt-auto flex flex-col gap-2 bg-purple-400">
@@ -42,14 +39,14 @@ function WebsiteItem({ website }: WebsiteItemProps) {
                             {website.tags.length > MAX_TAGS_TO_DISPLAY && <span className="text-secondary-700 text-sm font-semibold">+{website.tags.length - MAX_TAGS_TO_DISPLAY}</span>}
                         </div>
                         <div>
-                            <p className="text-neutral-500">Uploaded by: <a href="#" className="text-text-600 font-bold hover:text-text-700 transition-colors">{website.created_by}</a></p>
+                            <p className="text-neutral-500">Uploaded by: <a href={`/profile/${website.created_by}`} className="text-text-600 font-bold hover:text-text-700 transition-colors truncate max-w-2xs">{website.created_by}</a></p>
                         </div>
                     </div>
                     {/* <Button variant="primary">
                         Visit website <ExternalLink />
                     </Button> */}
                 </div>
-                <div className="flex flex-col items-end gap-2 bg-orange-300 w-fit ml-auto">
+                <div className="flex flex-col items-end gap-2 bg-orange-300 w-max">
                     {/* <img src={image} width={150} height={350} className="rounded-sm w-[80%] shrink" alt={`${name} screenshot`} /> */}
                     <WebsitePreview src={image} className="rounded-sm w-full grow" size={{ width: 150, height: 250 }} />
                     <Button variant={"secondary"} className="flex items-center justify-center cursor-pointer group gap-2">
