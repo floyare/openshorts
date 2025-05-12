49d0aa0a96d738f4bf381d2f27dc587937fa381c
floyare <neptuneeex@gmail.com>
2025-05-12 21:55:38 +0200

    fix: fixing website-item layout

--- src/components/websites/website-item.tsx
+++ src/components/websites/website-item.tsx
@@ -67,13 +67,13 @@ function WebsiteItem({ website, highlightedText = [] }: WebsiteItemProps) {
     }
 
     return (
-        <div className="px-6 py-4 max-w-lg rounded-sm border-[1px] border-background-800 bg-white w-full flex flex-col gap-2 grow relative">
+        <div className="px-6 py-4 2xl:max-w-lg max-w-full rounded-sm border-[1px] border-background-800 bg-white w-full flex flex-col gap-2 grow relative">
             <div className="grid sm:grid-cols-[1fr_auto] grid-cols-1 gap-1 !w-full">
-                <div className="flex flex-col gap-2 w-full">
+                <div className="flex flex-col gap-2 w-full bg-blue-400">
                     <a href={website.url} target="_blank" className="flex items-center gap-4 cursor-pointer hover:bg-primary-700/20 transition-colors rounded-sm w-full">
                         <WebsiteIcon src={`https://s2.googleusercontent.com/s2/favicons?domain=${url}&sz=128`} alt={`${name} favicon`} size={48} />
-                        <h2 className="font-bold text-2xl flex items-center gap-1 relative w-full">
-                            <span className="truncate inline-block !max-w-[160px]">{highlight(name)}</span>
+                        <h2 className="font-bold text-2xl flex items-center gap-1 relative w-full bg-orange-500">
+                            <span className="truncate inline-block 2xl:!max-w-[7.5vw] !max-w-[12rem] bg-red-500">{highlight(name)}</span>
                             <ExternalLink className="text-text-600" size={18} />
                         </h2>
                     </a>
@@ -96,8 +96,8 @@ function WebsiteItem({ website, highlightedText = [] }: WebsiteItemProps) {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col items-end gap-2 sm:w-max w-full">
-                    <WebsitePreview src={image ?? ""} className="rounded-sm grow w-full" size={{ width: 150, height: 250 }} />
+                <div className="flex flex-col items-end gap-2 sm:w-max w-full bg-green-300">
+                    <WebsitePreview src={image ?? ""} className="rounded-sm grow w-full" size={{ width: 120, height: 200 }} />
                     <Button variant={"secondary"} disabled={likeActionPending || !canBeLiked} onClick={() => likeActionPendingSet(handleLike)} className="relative flex items-center justify-center cursor-pointer group gap-2 border-[1px] border-secondary-500">
                         {
                             likeActionPending ? <LoaderCircle className="animate-spin" /> :
