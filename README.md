e623d770b21095efeb9516159b82cc703a648870
floyare <neptuneeex@gmail.com>
2025-05-24 21:34:57 +0200

    feat: added sitemap, robots.txt

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ todo:
 - [X] add website preview zoom on hover or click
 - [X] add list of uploaded websites with option to update the image preview
 - [ ] add notify when image preview is not uploaded
-- [ ] fix websites item layout on 2 columns and 3 columns
+- [X] fix websites item layout on 2 columns and 3 columns
 - [X] gemini ai rekomendacje po wpisaniu czego sie szuka i filtruje po największym matchu i ilosci like'ow
 - [ ] google adsy?
-- [ ] komentarze
+- [X] komentarze
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,21 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+//import node from '@astrojs/node';
 
 import vercel from '@astrojs/vercel';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()],
+  site: "https://openshorts.dev",
+  integrations: [react(), sitemap({
+    filter: (page) => !page.includes('/admin') && !page.includes('/layout')
+  })],
 
   adapter: vercel(),//node({mode: "standalone"}),
   experimental: {
