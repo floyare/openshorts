3bc62b796ffdeab39d5786b03bddbb35c128197b
floyare <neptuneeex@gmail.com>
2025-05-27 09:35:41 +0200

    refactor: changed ratelimit to fixedWindow (cheaper)

--- src/lib/ratelimiter.ts
+++ src/lib/ratelimiter.ts
@@ -1,9 +1,12 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { redis } from "./prisma";
 
+// TODO: w razie problemu zamienić ratelimit na arcjet
+// https://github.com/arcjet/arcjet-js/blob/main/examples/astro-integration/src/actions/index.ts
+
 const ratelimit = new Ratelimit({
     redis: redis,
-    limiter: Ratelimit.tokenBucket(1, "3s", 5),
+    limiter: Ratelimit.fixedWindow(5, "5s"),//Ratelimit.tokenBucket(1, "3s", 5),
     prefix: "ops-ratelimit",
 });
 
