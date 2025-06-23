659d082b2e5073e7c5a54a8199fe8327a7fdc781
floyare <neptuneeex@gmail.com>
2025-06-23 22:51:54 +0200

    feat: added theme-switcher button title

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -52,7 +52,7 @@ export function ThemeSwitcher() {
     };
 
     return (
-        <Button variant="default" className="border-[1px] border-primary-500" size="icon" onClick={toggleTheme}>
+        <Button variant="default" title={`Switch theme to ${currentTheme === "dark" ? "light" : "dark"}`} className="border-[1px] border-primary-500" size="icon" onClick={toggleTheme}>
             <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
         </Button>
