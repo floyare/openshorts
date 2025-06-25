0e886d94509597df4a580a7b1d64cd2895d172ac
floyare <neptuneeex@gmail.com>
2025-06-25 22:14:43 +0200

    test: working on auth optimizations

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,19 @@
 import { defineMiddleware } from "astro:middleware";
 import { auth } from "./lib/auth";
+import { debugLog } from "./lib/log";
 
 export const onRequest = defineMiddleware(async (context, next) => {
+    const authPaths = ['/', '/profile', '/website'];
+    if (!authPaths.some(p => new URL(context.request.url).pathname.startsWith(p))) {
+        return next();
+    }
+
     const isAuthed = await auth.api
         .getSession({
             headers: context.request.headers,
         })
+
+    debugLog("SUCCESS", "Requested session", !!isAuthed?.user)
 
     if (isAuthed) {
         context.locals.user = isAuthed.user;
