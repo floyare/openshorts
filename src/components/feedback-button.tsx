ad56dd6c538f44f7295f5743ba684f76bc37fb4b
floyare <neptuneeex@gmail.com>
2025-06-26 09:37:11 +0200

    test: working on performance tests

--- src/components/feedback-button.tsx
+++ src/components/feedback-button.tsx
@@ -4,6 +4,7 @@ import { useDialogManager } from "easy-dialogs";
 import { dialogs } from "@/lib/dialogs";
 
 // TODO: cos tu zfixowac bo sie robi drzewo renderow czy cos w googlu przez to
+// przez to ze jest importowany easy-dialogs tutaj i w innych komponentach to te inne komponenty tu daje
 const FeedbackButton = () => {
     const { callDialog } = useDialogManager(dialogs)
     return (
