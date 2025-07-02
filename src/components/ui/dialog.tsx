99a4779846abe9c2cc618e296e1421ce4718d631
floyare <neptuneeex@gmail.com>
2025-07-02 11:30:09 +0200

    style: added dialog overlay blur effect

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -35,7 +35,7 @@ function DialogOverlay({
         <DialogPrimitive.Overlay
             data-slot="dialog-overlay"
             className={cn(
-                "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-[1001] bg-black/60",
+                "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-[1001] bg-black/60 backdrop-blur-xs",
                 className
             )}
             {...props}
@@ -54,7 +54,7 @@ function DialogContent({
             <DialogPrimitive.Content
                 data-slot="dialog-content"
                 className={cn(
-                    "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-[1002] grid w-max max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg duration-200 md:max-w-5xl !my-4",
+                    "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-[1002] grid w-max max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg duration-200 md:max-w-5xl !my-4 drop-shadow-xl drop-shadow-background-50/80",
                     className
                 )}
                 {...props}
