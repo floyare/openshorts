e5b5a7c31d0a4294e617d54ac3ef5eff852f8590
floyare <neptuneeex@gmail.com>
2025-07-06 12:44:05 +0200

    style: changed blog posts list styles

--- src/components/blog/blog-posts.tsx
+++ src/components/blog/blog-posts.tsx
@@ -4,6 +4,7 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import Container from "../container";
 import { MultiSelect } from "../ui/multi-select";
+import { cn } from "@/lib/utils";
 
 type BlogPostsProps = {
     posts: any[]
@@ -84,53 +85,57 @@ const BlogPosts = (props: BlogPostsProps) => {
                     </div>
                 </label>
             </div>
-            <div className="flex flex-col items-center justify-center flex-wrap gap-2 p-4">
+            <div className="flex flex-wrap gap-2 p-4">
                 {filteredPosts.length === 0 && (
                     <div className="text-neutral-500 py-8">No posts found.</div>
                 )}
-                {filteredPosts.map((post: any) => (
-                    <a
-                        key={post.file}
-                        href={"/blog/" + getSlug(post.file)}
-                        className="flex items-center relative overflow-hidden md:min-w-xl min-w-full bg-white dark:bg-neutral-950 px-8 py-6 rounded-md no-underline gap-8 border-[1px] border-transparent hover:border-primary-500 transition-colors"
-                    >
-                        <div className="flex flex-col gap-3">
-                            <p className="text-neutral-500 dark:text-neutral-600">
-                                {format(
-                                    post.frontmatter.pubDate,
-                                    "dd.MM.yyyy"
-                                )}
-                            </p>
-                            <div className="space-y-2">
-                                <b className="flex items-center gap-2 text-xl max-w-md text-balance break-words">
-                                    {post.frontmatter.title}{" "}
-                                </b>
-                                <p className="max-w-md text-balance break-words">
-                                    {post.frontmatter.description}
+                {filteredPosts.map((post: any, index: number) => {
+                    const isFirst = index === 0
+                    return (
+                        <a
+                            key={post.file}
+                            href={"/blog/" + getSlug(post.file)}
+                            className={cn("flex grow items-center relative md:min-w-xl min-w-full bg-white dark:bg-neutral-950 px-8 py-6 rounded-md no-underline gap-8 border-[1px] border-transparent hover:border-primary-500 transition-colors", isFirst ? "border-primary-400" : "")}
+                        >
+                            {isFirst && <div className="absolute -top-4 -right-4 bg-primary-500 font-bold px-6 py-2 text-white rounded-full z-10">NEW</div>}
+                            <div className="flex flex-col gap-3">
+                                <p className="text-neutral-500 dark:text-neutral-600">
+                                    {format(
+                                        post.frontmatter.pubDate,
+                                        "dd.MM.yyyy"
+                                    )}
                                 </p>
+                                <div className="space-y-2">
+                                    <b className="flex items-center gap-2 text-xl max-w-md text-balance break-words">
+                                        {post.frontmatter.title}{" "}
+                                    </b>
+                                    <p className="max-w-md text-balance break-words">
+                                        {post.frontmatter.description}
+                                    </p>
+                                </div>
+                                <ul className="flex items-center gap-2 flex-wrap max-w-md">
+                                    {post.frontmatter.tags.map((tag: string) => (
+                                        <li key={tag}>
+                                            <div
+                                                className={`rounded-full px-4 py-1 ${selectedTags.includes(tag)
+                                                    ? "bg-primary-500 text-white"
+                                                    : "bg-secondary-600 text-white"
+                                                    }`}
+                                            >
+                                                {tag}
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
                             </div>
-                            <ul className="flex items-center gap-2 flex-wrap max-w-md">
-                                {post.frontmatter.tags.map((tag: string) => (
-                                    <li key={tag}>
-                                        <div
-                                            className={`rounded-full px-4 py-1 ${selectedTags.includes(tag)
-                                                ? "bg-primary-500 text-white"
-                                                : "bg-secondary-600 text-white"
-                                                }`}
-                                        >
-                                            {tag}
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                        <img
-                            className="relative lg:block hidden w-3xs z-0"
-                            src={post.frontmatter.image}
-                            alt={post.frontmatter.title}
-                        />
-                    </a>
-                ))}
+                            <img
+                                className="relative lg:block hidden w-3xs z-0 ml-auto"
+                                src={post.frontmatter.image}
+                                alt={post.frontmatter.title}
+                            />
+                        </a>
+                    )
+                })}
             </div>
         </>
     );
