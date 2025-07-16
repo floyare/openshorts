55f9cec25efdfc1cfb8acc3b17e0e49eab10b111
floyare <neptuneeex@gmail.com>
2025-07-17 01:55:32 +0200

    feat: added get-profiles /api endpoint

---
+++ src/pages/api/get-profiles.ts
@@ -0,0 +1,17 @@
+export const prerender = false
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async (ctx) => {
+    const key = ctx.request.headers.get("x-key")
+    if (key !== import.meta.env.PROFILE_FETCH_KEY) {
+        return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const profiles = await prisma?.user.findMany({})
+
+    return Response.json([
+        (profiles ? profiles.map((profile) => {
+            return `${import.meta.env.SITE}/profile/${profile.name}`;
+        }) : [])
+    ])
+};
