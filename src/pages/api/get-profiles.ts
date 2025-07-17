8186dd32bb7e287eaed9fe0182b6ec88e36f553a
floyare <neptuneeex@gmail.com>
2025-07-17 02:02:08 +0200

    fix: fixing /api/get-profiles

--- src/pages/api/get-profiles.ts
+++ src/pages/api/get-profiles.ts
@@ -1,5 +1,6 @@
 export const prerender = false
 import type { APIRoute } from "astro";
+import { prisma } from "../../lib/prisma"
 
 export const GET: APIRoute = async (ctx) => {
     const key = ctx.request.headers.get("x-key")
