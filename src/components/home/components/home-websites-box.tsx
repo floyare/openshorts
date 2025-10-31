fb5712e6c7a5f1debcf51d1ca7d3bcad2daf1971
floyare <neptuneeex@gmail.com>
2025-10-31 23:11:05 +0100

    refactor: minor color changes

--- src/components/home/components/home-websites-box.tsx
+++ src/components/home/components/home-websites-box.tsx
@@ -1,6 +1,6 @@
 const HomeWebsitesBox = ({ icons }: { icons: { url: string }[] }) => {
     return (
-        <div className="grid grid-cols-9 grid-rows-4 gap-6 bg-white p-4 rounded-md border-1 border-neutral-200">
+        <div className="grid grid-cols-9 grid-rows-4 gap-6 bg-white dark:bg-neutral-900 dark:border-neutral-700 p-4 rounded-md border-1 border-neutral-200">
             {icons.map((_, index) => (
                 <a href={_.url} target="_blank" key={index} className="bg-neutral-400 rounded-sm hover:scale-105 transition-transform">
                     {/* <WebsiteIcon src={`https://s2.googleusercontent.com/s2/favicons?domain=${_.url}&sz=128`} alt="Website Icon" /> */}
