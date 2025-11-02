daa5ddb032316d9611aaf9f524c355e73f13b952
floyare <neptuneeex@gmail.com>
2025-11-02 11:43:17 +0100

    refactor: added responsive layout in home page

--- src/components/home/components/home-websites-box.tsx
+++ src/components/home/components/home-websites-box.tsx
@@ -1,10 +1,10 @@
 const HomeWebsitesBox = ({ icons }: { icons: { url: string }[] }) => {
     return (
-        <div className="grid grid-cols-9 grid-rows-4 gap-6 bg-white dark:bg-neutral-900 dark:border-neutral-700 p-4 rounded-md border-1 border-neutral-200">
+        <div className="grid md:grid-cols-9 grid-cols-8 grid-rows-4 md:gap-6 gap-3 bg-white dark:bg-neutral-900 dark:border-neutral-700 p-4 rounded-md border-1 border-neutral-200">
             {icons.map((_, index) => (
                 <a href={_.url} target="_blank" key={index} className="bg-neutral-400 rounded-sm hover:scale-105 transition-transform">
                     {/* <WebsiteIcon src={`https://s2.googleusercontent.com/s2/favicons?domain=${_.url}&sz=128`} alt="Website Icon" /> */}
-                    <img src={`https://s2.googleusercontent.com/s2/favicons?domain=${_.url}&sz=128`} alt="Website Icon" width={48} height={48} className="h-12 w-12 object-contain rounded-sm" loading="lazy" />
+                    <img src={`https://s2.googleusercontent.com/s2/favicons?domain=${_.url}&sz=128`} alt="Website Icon" width={48} height={48} className="md:h-12 md:w-12 h-8 w-8 object-contain rounded-sm" loading="lazy" />
                 </a>
             ))}
         </div>
