3ce5118b6f0dd38e2729c8deca26a22183224366
floyare <neptuneeex@gmail.com>
2025-11-26 21:00:26 +0100

    refactor: upgraded to prisma v7

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,21 @@
-import { PrismaClient } from "@prisma/client"
+//import { PrismaClient } from "@prisma/client"
 import { Redis } from "@upstash/redis";
 
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from "@/generated/client";
 export let prisma: PrismaClient;
 
+const adapter = new PrismaPg({
+    connectionString: import.meta.env.DATABASE_URL
+});
+
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
+    prisma = new PrismaClient({ adapter });
 } else {
     type GlobalWithPrisma = typeof globalThis & { prisma?: PrismaClient };
     const globalWithPrisma = globalThis as GlobalWithPrisma;
     if (!globalWithPrisma.prisma) {
-        globalWithPrisma.prisma = new PrismaClient();
+        globalWithPrisma.prisma = new PrismaClient({ adapter });
     }
     prisma = globalWithPrisma.prisma;
 }
