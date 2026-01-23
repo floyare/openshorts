cd38be25ef526cb27d51432a4f754f7edc0dff46
floyare <neptuneeex@gmail.com>
2026-01-23 12:17:47 +0100

    feat: added debug error

--- src/components/website-browser.tsx
+++ src/components/website-browser.tsx
@@ -360,7 +360,13 @@ const WebsiteBrowser = ({ /*entryWebsites, totalWebsites, tags,*/ currentUser }:
                 </Container>
                 {!isMobile && <Container className="bg-primary-500 overflow-hidden relative z-10 border-[1px] border-primary-400 text-white grid place-items-center-safe gap-3">
                     <div className="flex flex-col items-center justify-center text-center">
-                        <h2 className="font-semibold text-xl tracking-tight">Help <b>openshorts</b> go global!</h2>
+                        <h2 className="font-semibold text-xl tracking-tight">Help <b onDoubleClick={() => sendEvent("error", {
+                            message: "Test debug error",
+                            details: {
+                                currentUser
+                            },
+                            caller: "Easter egg!"
+                        })}>openshorts</b> go global!</h2>
                         <p className="">Share the website with your friends and followers to help us grow!</p>
                     </div>
                     <div className="flex items-center flex-wrap gap-2 justify-center relative z-10">
