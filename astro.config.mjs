089df69b702178363610d08d0b01f99ede9d7ad9
floyare <neptuneeex@gmail.com>
2026-04-25 19:35:41 +0200

    fix: fixed seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,69 +14,70 @@ import mdx from '@astrojs/mdx';
 //const customPages = await fetch("https://example.com").then(users => { return ["https://openshorts.dev/XD"] });
 import { loadEnv } from "vite";
 
+import playformCompress from "@playform/compress";
+
+import playformInline from "@playform/inline";
+
 //@ts-ignore
 const { PROFILE_FETCH_KEY } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
     build: {
-        inlineStylesheets: 'always', 
+        inlineStylesheets: 'auto', 
     },
     vite: {
         plugins: [tailwindcss()],
         build: {
+            sourcemap: true,
             rollupOptions: {
                 treeshake: 'smallest',
             },
         },
     },
     site: "https://www.openshorts.dev",
-    integrations: [
-        react(),
-        sitemap({
-            customSitemaps: ["https://www.openshorts.dev/sitemap-websites.xml"],
-            customPages: await fetch("https://www.openshorts.dev/api/get-profiles", {headers: {
-                'x-key': PROFILE_FETCH_KEY
-            }}).then(async(users) => { 
-                const j = await users.json(); 
-                console.log("Custom pages fetched:", j[0], [...(j[0])] );
-                return [...(j[0])] 
-            }).catch((err) => {
-                console.warn("Tried to get env:", PROFILE_FETCH_KEY);
-                console.error("Error fetching custom pages:", err);
-                return [];
-            }),
-            filter: (page) =>
-                !page.includes('/admin') &&
-                !page.includes('/layout') &&
-                !page.includes('/signout') &&
-                !page.endsWith("/profile/"),
-            serialize(item) {
-                if (!item.url) return item;
-                let priority = 0.4;
-                if (item.url === "https://www.openshorts.dev/" || item.url === "https://www.openshorts.dev") {
-                    priority = 1.0;
-                } else if (item.url === ("https://www.openshorts.dev/browse")) {
-                    priority = 0.9;
-                } else if (item.url.startsWith("https://www.openshorts.dev/browse/")) {
-                    priority = 0.8;
-                } else if (item.url.startsWith("https://www.openshorts.dev/profile/")) {
-                    priority = 0.5;
-                } else if (item.url.startsWith("https://www.openshorts.dev/blog/")) {
-                    priority = 0.6;
-                } else if (item.url === "https://www.openshorts.dev/blog") {
-                    priority = 0.7;
-                }
-                return { ...item, priority };
-            }
+    integrations: [react(), // partytown({
+    sitemap({
+        customSitemaps: ["https://www.openshorts.dev/sitemap-websites.xml"],
+        customPages: await fetch("https://www.openshorts.dev/api/get-profiles", {headers: {
+            'x-key': PROFILE_FETCH_KEY
+        }}).then(async(users) => { 
+            const j = await users.json(); 
+            console.log("Custom pages fetched:", j[0], [...(j[0])] );
+            return [...(j[0])] 
+        }).catch((err) => {
+            console.warn("Tried to get env:", PROFILE_FETCH_KEY);
+            console.error("Error fetching custom pages:", err);
+            return [];
         }),
-        // partytown({
-        //     config: {
-        //             forward: ['dataLayer.push'],
-        //     }
-        // }),
-        mdx(),
-    ],
+        filter: (page) =>
+            !page.includes('/admin') &&
+            !page.includes('/layout') &&
+            !page.includes('/signout') &&
+            !page.endsWith("/profile/"),
+        serialize(item) {
+            if (!item.url) return item;
+            let priority = 0.4;
+            if (item.url === "https://www.openshorts.dev/" || item.url === "https://www.openshorts.dev") {
+                priority = 1.0;
+            } else if (item.url === ("https://www.openshorts.dev/browse")) {
+                priority = 0.9;
+            } else if (item.url.startsWith("https://www.openshorts.dev/browse/")) {
+                priority = 0.8;
+            } else if (item.url.startsWith("https://www.openshorts.dev/profile/")) {
+                priority = 0.5;
+            } else if (item.url.startsWith("https://www.openshorts.dev/blog/")) {
+                priority = 0.6;
+            } else if (item.url === "https://www.openshorts.dev/blog") {
+                priority = 0.7;
+            }
+            return { ...item, priority };
+        }
+    }), //     config: {
+    //             forward: ['dataLayer.push'],
+    //     }
+    // }),
+    mdx(), , playformInline(), playformCompress()],
     trailingSlash: 'never',
     output: 'static',
     adapter: vercel(),//node({mode: "standalone"}),
@@ -85,7 +86,7 @@ export default defineConfig({
             provider: fontProviders.google(),
             name: "Inter",
             cssVariable: "--font",
-            weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+            weights: [300, 400, 500, 600, 700, 800, 900],
         }]
     }
 });
